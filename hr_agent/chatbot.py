c4078a5d31979ee2741832268efb733c76350993
Olivier Hassanaly <olivierhass@gmail.com>
2025-09-19 15:38:45 +0200

    adding a user interface with streamlit

--- hr_agent/chatbot.py
+++ hr_agent/chatbot.py
@@ -31,7 +31,24 @@ def stream_graph_updates(user_input: str):
             if ai_msgs:
                 print("Olivier:", ai_msgs[-1].content)
 
-            # print("Olivier:", value["messages"][-1].content)
+def stream_graph_updates_ui(user_input: str):
+    config = {"configurable": {"thread_id": "1"}}  # any thread id value works here
+    for event in graph.stream(
+        {
+            "messages": [
+                {"role": "system", "content": SYSTEM_PROMPT},
+                {"role": "user", "content": user_input},
+            ]
+        },
+        config,
+    ):
+        for value in event.values():
+            msgs = value["messages"]
+            # Only pick AI messages (the model's replies)
+            ai_msgs = [m for m in msgs if is_final_ai(m)]
+            if ai_msgs:
+                yield(ai_msgs[-1].content)
+
 
 
 if __name__ == "__main__":
---
+++ hr_agent/ui.py
@@ -0,0 +1,38 @@
+from chatbot import *
+import streamlit as st
+
+st.set_page_config(page_title="AI enginner interview", page_icon="💬", layout="centered")
+
+# with st.chat_message("Olivier"):
+#     st.write("Hello, I can answer any question for that AI engineer interview")
+
+# prompt = st.chat_input("Say something")
+# if prompt:
+#     st.write(f"User has sent the following prompt: {prompt}")
+
+# st.title("Simple chat")
+
+st.title("AI enginner interview")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("Tell me"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        response = st.write_stream(stream_graph_updates_ui(prompt))
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
